b47c18b2d5f9d4a3a1ccef9eeee67055cd902fe5
serom-kim <142819677+serom-kim@users.noreply.github.com>
2024-07-03 22:09:44 -0500

    finished button.py

--- button.py
+++ button.py
@@ -1,5 +1,19 @@
 import pygame
+import sys
 from pygame.locals import *
+
+#define colors
+white = (255, 255, 255)
+bliue = (0, 0, 255)
+green = (0, 255, 0)
+red = (255, 0, 0)
+black = (0, 0, 0)
+
+#set up display
+width, height = 1000, 700
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Button Example')
+
 
 class Button: #rectangular button
     def __init__(self, x, y, width, height, text, color, hover_color):
@@ -8,4 +22,41 @@ class Button: #rectangular button
         self.color = color
         self.hover_color = hover_color
         self.font = pygame.font.Font(None, 36) #None is default font and 36 is how big
+        self.text_surf = self.font.render(text, True, white)
+        self.text_rect = self.text_surf.get_rect(center = self.rect.center)
+    
+    def draw(self, screen):
+        mousepos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mousepos):
+            pygame.draw.rect(screen, self.hover_color, self.rect)
         
+        else:
+            pygame.draw.rect(screen, self.color, self.rect)
+        text_surf = self.font.render(self.text, True, white)
+        text_rect = text_surf.get_rect(center = self.rect.center)
+        screen.blit(text_surf, text_rect)
+    
+    def is_clicked(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                return True
+        return False
+
+def main():
+    button = Button(350, 250, 100, 50, 'Click Me', (255, 0, 0), (200, 0, 0))
+    
+    while True:
+        screen.fill(black)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if button.is_clicked(event):
+                print('Button clicked :)')
+        
+        button.draw(screen)
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    main()
