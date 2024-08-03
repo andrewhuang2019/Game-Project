ea0995a6b6cf4d66394c242581329f4cc153403f
andrewhuang2019 <andrewhuang2019@gmail.com>
2024-08-03 16:28:43 -0500

    Update screenmanager.py

--- screens/screenmanager.py
+++ screens/screenmanager.py
@@ -3,8 +3,8 @@ class ScreenManager:
     def __init__(self):
         self.cur_screen = None
 
-    def is_blending_button_clicked(self, blending_screen):
-        if self.cur_screen.blending_button_is_clicked():
+    def is_blending_button_clicked(self, blending_screen, event):
+        if self.cur_screen.blending_button_is_clicked(event):
             blending_screen.make_current_screen()
             self.cur_screen.end_current_screen()
             self.cur_screen = blending_screen
