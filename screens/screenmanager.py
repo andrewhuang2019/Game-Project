96500309b55d0e0f9e343d177f9f488d7e60e94d
serom-kim <142819677+serom-kim@users.noreply.github.com>
2024-08-03 16:39:16 -0500

    fixed page navigation

--- screens/screenmanager.py
+++ screens/screenmanager.py
@@ -3,9 +3,27 @@ class ScreenManager:
     def __init__(self):
         self.cur_screen = None
 
+    def is_building_button_clicked(self, building_screen, event):
+        if self.cur_screen.building_button_is_clicked(event):
+            building_screen.make_current_screen()
+            self.cur_screen.end_current_screen()
+            self.cur_screen = building_screen
+
     def is_blending_button_clicked(self, blending_screen, event):
         if self.cur_screen.blending_button_is_clicked(event):
             blending_screen.make_current_screen()
             self.cur_screen.end_current_screen()
             self.cur_screen = blending_screen
+    
+    def is_topping_button_clicked(self, topping_screen, event):
+        if self.cur_screen.topping_button_is_clicked(event):
+            topping_screen.make_current_screen()
+            self.cur_screen.end_current_screen()
+            self.cur_screen = topping_screen
+    
+    def is_serving_button_clicked(self, serving_screen, event):
+        if self.cur_screen.serving_button_is_clicked(event):
+            serving_screen.make_current_screen()
+            self.cur_screen.end_current_screen()
+            self.cur_screen = serving_screen
     
